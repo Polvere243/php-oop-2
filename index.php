c3501fe2db00d9b7a09aaa939df475d5ad074d0c
Polvere243 <danielercoli@gmail.com>
2024-02-29 00:39:17 +0100

    added readme and created boilerplate

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Daniele Ercoli">
+    <meta name="description" content="esercizio">
+    <meta name="keyword" content="php, css, markdown">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Commerce</title>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+    
+</body>
+</html>
