1712a2d007e7206412cbbf4fd7772aa073ab352e
Polvere243 <danielercoli@gmail.com>
2024-02-29 15:18:20 +0100

    fixed class product

--- models/Product.php
+++ models/Product.php
@@ -3,16 +3,12 @@
 class product {
 // variabili d'istanza
 public $name;
-public $ingredients = [];
 public $price;
-public $weight;
 
 
 // costruttore 
-public function __construct ($name, $ingredients, $price, $weight) {
+public function __construct ($name, $price) {
     $this ->name = $name;
-    $this ->ingredients = $ingredients;
     $this ->price = $price;
-    $this ->weight = $weight;
 }
 }
