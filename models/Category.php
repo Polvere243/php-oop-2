12b1a3d60fb8ca7252ef7a5175fead382abec231
Polvere243 <danielercoli@gmail.com>
2024-02-29 15:07:58 +0100

    created a class category

--- models/Category.php
+++ models/Category.php
@@ -1,2 +1,13 @@
 <?php
+// classe
+class Category {
+// variabili d'istanza
+public $label;
+public $icon;
 
+// costruttore 
+public function __construct ($label, $icon) {
+    $this ->label = $label;
+    $this ->icon =$icon;
+}
+}
