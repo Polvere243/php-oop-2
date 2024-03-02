6642bae26b32f6be8f11ba5934f6899036c9abeb
Polvere243 <danielercoli@gmail.com>
2024-03-02 17:40:42 +0100

    added comments

--- models/Category.php
+++ models/Category.php
@@ -12,23 +12,24 @@ public function __construct ($label, $icon)
     $this ->setIcon ($icon);
 }
 
+// getter per l'etichetta
 public function getLabel($label) 
 {
     return $this ->label; 
 }
-
+// setter per l'etichetta
 public function setLabel ($label)
 {
     return $this ->label = $label;
 }
 
-
+// getter per l'icona
 public function getIcon($icon) 
 {
     return $this ->icon; 
 }
 
-
+// setter per l'icona
 public function setIcon ($icon)
 {
     return $this ->icon = $icon;
