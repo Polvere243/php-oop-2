532d893e15fee142cc5ccb0d40aec4b9290a7f59
Polvere243 <danielercoli@gmail.com>
2024-03-03 00:14:08 +0100

    added a comment

--- data/products.php
+++ data/products.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../models/products/Food.php';
 require_once __DIR__ . '/../models/products/Toy.php';
 require_once __DIR__ . '/../models/products/Accessory.php';
 
-
+// dichiaro un array coi prodotti
 $products = [
     new Food (
         'Royal Canin Mini Adult',
