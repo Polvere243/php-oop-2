d174cd873e10579ac4a2bd0676f8f2a6f568fdbb
Polvere243 <danielercoli@gmail.com>
2024-03-02 17:06:25 +0100

    put variables as private and declared setter and getter methods

--- models/Category.php
+++ models/Category.php
@@ -1,13 +1,38 @@
 <?php
 // classe
-class Category {
+class Category  {
 // variabili d'istanza
-public $label;
-public $icon;
+private $label;
+private $icon;
 
 // costruttore 
-public function __construct ($label, $icon) {
+public function __construct ($label, $icon) 
+{
     $this ->label = $label;
     $this ->icon =$icon;
 }
+
+public function getLabel($label) 
+{
+    return $this ->label; 
+}
+
+public function setLabel ($label)
+{
+    return $this ->label = $label;
+}
+
+
+public function getIcon($icon) 
+{
+    return $this ->icon; 
+}
+
+
+public function setIcon ($icon)
+{
+    return $this ->icon = $icon;
+}
+
+
 }
