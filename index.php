ead71fe982c8f5301aa7365e72679fd68b8089b1
Polvere243 <danielercoli@gmail.com>
2024-03-03 01:06:36 +0100

    created three different arrays by using filter

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 
-<?php ?>
+<?php
+// importo il file coi prodotti
+require __DIR__. '/data/products.php';
+
+// filtro gli array
+
+$foods = array_filter($products, fn($p) =>$::type === 'food');
+$toys = array_filter($products, fn($p) =>$::type === 'toy');
+$accessories = array_filter($products, fn($p) =>$::type === 'accessory');
+
+?>
 
 
 <!DOCTYPE html>
@@ -9,7 +19,7 @@
     <meta charset="UTF-8">
     <meta name="author" content="Daniele Ercoli">
     <meta name="description" content="esercizio">
-    <meta name="keyword" content="php, css, markdown">
+    <meta name="keyword" content="php, css, markdown, html">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-Commerce</title>
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css' integrity='sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==' crossorigin='anonymous'/>
